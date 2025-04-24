5fba800041f7d61e14a42704a795be8499d48dbc
TimoElony <timo@elony.de>
2025-04-25 00:57:40 +0300

    cleaned up a few unused components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import TopSpot from './components/TopSpot.tsx'
-import Nav from './components/Nav.tsx'
+
 import Dashboard from './components/Dashboard.tsx'
 
 
@@ -7,18 +6,9 @@ function App() {
 
   return (
     <div className="min-h-screen min-w-screen flex flex-col gap-4 bg-gray-100 px-4">
-      <header className="flex justify-between items-center py-2">
-        <TopSpot />
-        <Nav />
-      </header>
       <main>
         <Dashboard/>
       </main>
-      <footer className='grid grid-cols-3 align-middle gap-4 p-4 bg-gray-200'>
-        <div/>
-        <div/>
-        <p className="text-sm">Copyright</p>
-      </footer>
     </div>
   )
 }
--- src/components/TopSpot.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function TopSpot () {
-
-    return (
-        <div>
-        <img src="/logo.png" alt="Logo" className="w-20 h-20" />
-        </div>
-    );
-}
