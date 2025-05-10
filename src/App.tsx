dbecc7d9bd48d3efbe27cb3775db1e11f9cf9c96
TimoElony <timo.elony@gmail.com>
2025-05-10 23:29:25 +0300

    no more mx auto

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <header>
         <Login loggedIn={!(sessionToken==='')} setSessionToken={setSessionToken}/>
       </header>
-      <main className='lg:w-3xl mx-auto'>
+      <main className='lg:w-3xl'>
         <Dashboard sessionToken={sessionToken}/>
       </main>
     </div>
