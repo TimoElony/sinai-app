d7365ecda7e7a9a03ff23d5e69599dbbbc060769
TimoElony <timo.elony@gmail.com>
2025-09-26 20:28:44 +0200

    formatting size changed to allow route number

--- src/components/ClimbingRoutes.tsx
+++ src/components/ClimbingRoutes.tsx
@@ -197,7 +197,7 @@ export default function  ClimbingRoutes ({areas, areaDetails, selectedArea, onAr
                         </>
                         )} 
                         { sessionToken && changeRoutesNotLines && (
-                            <div className="flex flex-col md:flex-row md:items-center bg-gray-200 rounded-lg gap-2 mx-1 p-2">
+                            <div className="flex flex-col lg:flex-row md:items-center bg-gray-200 rounded-lg gap-2 mx-1 p-2">
                                 <select className="p-2 bg-white rounded-lg" value={selectedRoute?.id || " "} onChange={(e)=>setSelectedRoute(routes.find(route=>route.id === e.target.value))}>
                                     <option value=" ">Select route to add</option>
                                     {routes.map((route)=><option key={route.id} value={route.id}>{route.name}</option>)}
