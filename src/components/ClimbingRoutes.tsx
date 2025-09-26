39edfad72110dbf5cfdf91411d0ea3de62642401
TimoElony <timo.elony@gmail.com>
2025-09-26 21:39:45 +0200

    removed topo name from div with controls

--- src/components/ClimbingRoutes.tsx
+++ src/components/ClimbingRoutes.tsx
@@ -188,8 +188,8 @@ export default function  ClimbingRoutes ({areas, areaDetails, selectedArea, onAr
                 
             return (
                 <div key={topo.id} className="flex flex-col gap-2 max-w-full">
+                    <h2 >{topo.description}</h2>
                     <div className="flex flex-col lg:flex-row lg:items-center">
-                        <h2 >{topo.description}</h2>
                         { sessionToken && (
                         <>
                         <Switch id="routesInTopo" checked={changeRoutesNotLines} onCheckedChange={()=>setChangeRoutesNotLines(!changeRoutesNotLines)}/>
