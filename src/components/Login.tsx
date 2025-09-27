519d3e541e54b8f8ee5a8b1f051634c1d2e9b8bf
TimoElony <timo.elony@gmail.com>
2025-09-28 00:16:30 +0200

    removed signup function

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,33 +2,9 @@ import { useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
-
-
-const submitSignup = async (email: string, password: string) => {
-    try {
-        const response = await fetch('https://sinai-backend.onrender.com/auth/signup', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password,
-            }),
-        });
-
-        const data = await response.json();
-        console.log("Signup response:", data.message);
-        alert("Click on the link in your email to confirm your account");
-    } catch (error) {
-        console.error("Error signing up:", error);
-    }
-}
-
 export default function Login({loggedIn, setSessionToken}: {loggedIn: boolean; setSessionToken: React.Dispatch<React.SetStateAction<string>>}) {
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
-    const [toggleLogin, setToggleLogin] = useState<boolean>(true);
     const [hideAll, setHideAll] = useState<boolean>(true);
 
     const submitLogin  = async (email: string, password: string) => {
@@ -59,7 +35,7 @@ export default function Login({loggedIn, setSessionToken}: {loggedIn: boolean; s
             {(!loggedIn && !hideAll) ? (
                 <div className="flex flex-col md:flex-row md:items-center gap-2 m-2">
                         <Button onClick={()=>setHideAll(true)}>hide</Button>
-                        <h1>{toggleLogin ? 'Login' : 'Sign up'}</h1>
+                        <h1>Login</h1>
                         <label className="m-2" htmlFor="emailField">email*
                         <Input
                             type="email"
@@ -78,31 +54,10 @@ export default function Login({loggedIn, setSessionToken}: {loggedIn: boolean; s
                             className="border-2 border-gray-300 rounded-md p-2"
                         />
                         </label>
-                        <Button
-                            onClick={() => {
-                                if (toggleLogin){
+                        <Button onClick={() => {
                                     console.log("Login clicked");
-                                    submitLogin(email, password);
-                                } else {
-                                    console.log("Sign up clicked");
-                                    submitSignup(email, password);
-                                    setToggleLogin(!toggleLogin);
-                                    alert('Check your email to confirm Sign up')
-                                }
-                            }}
-                            
-                        >
-                            {toggleLogin? 'Login' : 'Sign up'}
-                        </Button>
-                        {toggleLogin && 
-                            <Button
-                                onClick={() => {
-                                    setToggleLogin(false);
-                                }}
-                            >
-                                No account yet?
-                            </Button>
-                        }   
+                                    submitLogin(email, password);                                
+                        }}>Login</Button>
                     </div>
             ): (
                 !hideAll &&
