94bb5a4eb431d49c703373c3d639b94859e6511b
Timo ELony <timo.elony@gmail.com>
2025-12-12 22:13:55 +0100

    apparently whitespace-nowrap is the better way

--- src/components/ClimbingRoutes.tsx
+++ src/components/ClimbingRoutes.tsx
@@ -224,10 +224,10 @@ export default function  ClimbingRoutes ({areas, areaDetails, selectedArea, onAr
                     <table className="table-auto w-full">
                         <thead>
                             <tr className="text-gray-700">
-                                <th className="text-start">No in Topo</th>
+                                <th className="text-start whitespace-nowrap">No in Topo</th>
                                 <th className="text-start">Name</th>
-                                <th className="text-end">Grade</th>
-                                <th className="text-end">Length</th>
+                                <th className="text-end whitespace-nowrap">Grade</th>
+                                <th className="text-end whitespace-nowrap">Length</th>
                             </tr>
                         </thead>
                     <tbody>
